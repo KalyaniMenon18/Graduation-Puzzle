a88b3c611c81822ca7293cadae12f444639c1b9f
Kalyani menon <pkalyanimenon@gmail.com>
2023-11-27 11:52:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import numpy as np
 
 if __name__ == "__main__":
 
-    #read from input file
+    # read input from the list
+    inputs = [5, 10]
+    for n in inputs:
+        obj = Attendance(n)
+        obj.printSolution()
+
+    # read from input file
     data = np.loadtxt('input.txt', dtype=int)
     for n in data:
         obj = Attendance(n)
